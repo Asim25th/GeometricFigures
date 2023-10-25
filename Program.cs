ddd33772a2bda5e8907ecd6c97e5d1e85b26b833
prdb <prdb@spb-rtk.ru>
2023-10-25 17:19:14 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -4,54 +4,54 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ПРОГРАММА ДЛЯ ВЫЧИСЛЕНИЯ ПЛОЩАДИ \nИ ПЕРИМЕТРА ГЕОМЕТРИЧЕСКИХ ФИГУР");
-            Console.WriteLine("\n1. Квадрат\n2. Прямоугольник\n3. Круг\n4. Треугольник\n5. Пирамида / Конус");
-            Console.Write("\nВведите номер желаемой фигуры: ");
-            int userChoice = (Convert.ToInt32(Console.ReadLine()));
-
-            switch (userChoice)
+            while (true)
             {
-                case 1:
-                    Square square = new Square(0, 0, 0);
-                    Console.Clear();
-                    square.Input();
-                    Console.Clear();
-                    Main(args);
-                    break;
-                case 2:
-                    Rectangle rectangle = new Rectangle(0, 0, 0);
-                    Console.Clear();
-                    rectangle.Input();
-                    Console.Clear();
-                    Main(args);
-                    break;
-                case 3:
-                    Circle circle = new Circle(0, 0, 0);
-                    Console.Clear();
-                    circle.Input();
-                    Console.Clear();
-                    Main(args);
-                    break;
-                case 4:
-                    Triangle triangle = new Triangle(0, 0, 0);
-                    Console.Clear();
-                    triangle.Input();
-                    Console.Clear();
-                    Main(args);
-                    break;
-                case 5:
-                    Pyramid pyramid = new Pyramid(0, 0, 0);
-                    Console.Clear();
-                    pyramid.Input();
-                    Console.Clear();
-                    Main(args);
-                    break;
-                default:
-                    Console.WriteLine("\nОШИБКА: Вы ввели неверное значение. Нажмите Enter и попробуйте снова.");
-                    Console.ReadKey();
-                    Console.Clear();
-                    Main(args);
-                    break;
+                Console.WriteLine("ПРОГРАММА ДЛЯ ВЫЧИСЛЕНИЯ ПЛОЩАДИ \nИ ПЕРИМЕТРА ГЕОМЕТРИЧЕСКИХ ФИГУР");
+                Console.WriteLine("\n1. Квадрат\n2. Прямоугольник\n3. Круг\n4. Треугольник\n5. Пирамида / Конус");
+                Console.Write("\nВведите номер желаемой фигуры: ");
+                int userChoice = (Convert.ToInt32(Console.ReadLine()));
+
+                switch (userChoice)
+                {
+                    case 0:
+                        System.Environment.Exit(0);
+                        break;
+                    case 1:
+                        Square square = new Square(0, 0, 0);
+                        Console.Clear();
+                        square.Input();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Rectangle rectangle = new Rectangle(0, 0, 0);
+                        Console.Clear();
+                        rectangle.Input();
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Circle circle = new Circle(0, 0, 0);
+                        Console.Clear();
+                        circle.Input();
+                        Console.Clear();
+                        break;
+                    case 4:
+                        Triangle triangle = new Triangle(0, 0, 0);
+                        Console.Clear();
+                        triangle.Input();
+                        Console.Clear();
+                        break;
+                    case 5:
+                        Pyramid pyramid = new Pyramid(0, 0, 0);
+                        Console.Clear();
+                        pyramid.Input();
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("\nОШИБКА: Вы ввели неверное значение. Нажмите Enter и попробуйте снова.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
             }
         }
     }
